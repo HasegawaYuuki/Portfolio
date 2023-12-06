e1fd8240a1d8fd5358d6fb5e31c3dfcb0161ac83
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-06 06:22:25 +0000

    devese初期設定終わり

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  
+  def after_sign_in_path_for(resource)
+    if resource == :admin
+      # 管理者側の遷移先
+    elsif rosource == :customer
+      # 顧客側の遷移先
+    end
+  end
+  
 end
