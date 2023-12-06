00defd42edace88c03af407a35110f746837412d
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-06 12:14:25 +0000

    ログイン可能ヘッダーも変わる

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -39,7 +39,7 @@ class Public::RelationshipsController < ApplicationController
   # end
 
   def after_sign_up_path_for(resource)
-    customers_mypage_path
+    customers_path
   end
 
   protected
