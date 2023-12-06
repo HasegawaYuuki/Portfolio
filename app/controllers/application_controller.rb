77e40b766025cba498f521f69f9a63ac63b11a7c
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-06 11:21:41 +0000

    ヘッダー一旦完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource == :admin
-      about_path
-    elsif rosource == :customer
-      about_path
+      root_path
+    elsif resource == :customer
+      root_path
     end
   end
 
