dc9f2e39d550df398c933fd368a4822a948f1185
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-07 05:27:34 +0000

    マイページタブ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,15 +2,37 @@ class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
 
   def index
+    @customer = current_customer
   end
 
   def show
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    customer = current_customer
+    if customer.update(customer_params)
+      flash[:edit] = "登録情報変更に成功しました。"
+      redirect_to customers_path
+    else
+      flash[:edit_danger] = "登録情報変更に失敗しました。"
+      redirect_to edit_customers_path
+    end
   end
-  
+
+  def withdraw
+    current_customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :introduction)
+  end
+
 end
