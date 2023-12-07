3a0cf2bb32cd91aebea63e2054766e5e4d51b854
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-07 12:38:13 +0000

    アソシエーション

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,2 +1,16 @@
 class Public::ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @review.end_customer_id = current_end_customer.id
+    # 受け取った値を,で区切って配列にする
+    tag_list = params[:review][:name].split(',')
+    if @review.save
+      @review.save_tags(tag_list)
+      redirect_to reviews_path, notice:'投稿が完了しました'
+    else
+      render :new
+    end
+  end
+  
+  
 end
