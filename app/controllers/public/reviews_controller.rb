3e5ef50277c29ea791b95d08b83bf921f8e7a036
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-08 04:28:51 +0000

    投稿詳細画面少し進めた

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -21,15 +21,32 @@ class Public::ReviewsController < ApplicationController
   end
 
   def show
+    @reviews = Review.find(params[:id])
+    @customer = @reviews.customer
   end
 
   def edit
+    @review = Review.find(params[:id])
+    @tag_list = @review.tags.pluck(:name).join(',')
   end
 
   def update
+    @review = Review.find(params[:id])
+    @review.customer_id = current_customer.id
+    # 受け取った値を,で区切って配列にする
+    tag_list = params[:review][:name].split(',')
+    if @review.update(review_params)
+      @review.save_tags(tag_list)
+      redirect_to reviews_path, notice:'投稿が完了しました'
+    else
+      render :new
+    end
   end
 
   def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to reviews_path
   end
 
   private
