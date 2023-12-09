47ffbdbbff8e3d323fefa270c760c42a66545fbc
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-09 05:43:31 +0000

    返信機能一旦削除

--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -4,15 +4,13 @@ class Public::ReviewCommentsController < ApplicationController
     @review = Review.find(params[:review_id])
     comment = current_customer.review_comments.new(review_comment_params)
     comment.review_id = @review.id
-    @comment_reply = @review.comments.new
     comment.save
     flash.now[:notice] = "コメントの投稿に成功しました。"
     redirect_back(fallback_location: review_path(@review.id))
   end
 
   def destroy
-    @review = Review.find(params[:review_id, :id])
-    @comment_reply = @review.comments.new
+    @review = Review.find(params[:id])
     @comment = ReviewComment.find(params[:id])
     @comment.destroy
     flash.now[:notice] = "コメントを削除しました。"
@@ -22,7 +20,7 @@ class Public::ReviewCommentsController < ApplicationController
   private
 
   def review_comment_params
-    params.require(:review_comment).permit(:comment)
+    params.require(:review_comment).permit(:comment, :customer_id, :review_id)
   end
 
 end
