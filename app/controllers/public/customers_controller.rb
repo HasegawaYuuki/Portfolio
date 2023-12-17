73f0cf7f4cbf342126541f7eda69ffc686e1ea8b
HasegawaYuuki <hasegawa990212@gmail.com>
2023-12-17 16:48:52 +0000

    退会機能未修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -40,6 +40,7 @@ class Public::CustomersController < ApplicationController
   def withdraw
     current_customer.update(is_active: false)
     reset_session
+    flash[:notice] = "退会処理を実行いたしました"
     redirect_to root_path
   end
 
@@ -55,7 +56,7 @@ class Public::CustomersController < ApplicationController
       redirect_to customer_path(current_customer.id)
     end
   end
-  
-  
+
+
 
 end
